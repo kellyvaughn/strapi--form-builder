d71d5f4123e6ee9e89a9f257179cb7ee57ceba90
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2020-09-10 08:11:01 -0600

    webpack window is not defined patch when building in universal

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  mode: 'production',
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   output: {
     path: path.resolve(__dirname) + "/dist",
-    library: 'StrapiFormBuilder',
-    libraryTarget: 'umd'
+    library: "StrapiFormBuilder",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+    globalObject: `(typeof self !== 'undefined' ? self : this)`,
   },
   devServer: {
-    contentBase: './'
+    contentBase: "./",
   },
-  watch: true
+  watch: true,
 };
